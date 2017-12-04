1d1cb95f96661da59679f92fcb4d51379ea1807d
ECasielles <flapspoiler@gmail.com>
2017-12-04 13:49:42 +0100

    Main template

---
+++ app/src/main/java/com/example/usuario/ad_ejercicios_t3/CreacionActivity.java
@@ -0,0 +1,13 @@
+package com.example.usuario.ad_ejercicios_t3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreacionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creacion);
+    }
+}
---
+++ app/src/main/java/com/example/usuario/ad_ejercicios_t3/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.usuario.ad_ejercicios_t3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnPartes:
+                startActivity(new Intent(this, PartesActivity.class));
+                break;
+            case R.id.btnNotas:
+                startActivity(new Intent(this, NotasActivity.class));
+                break;
+            case R.id.btnTitulares:
+                startActivity(new Intent(this, TitularesActivity.class));
+                break;
+            case R.id.btnNoticias:
+                startActivity(new Intent(this, NoticiasActivity.class));
+                break;
+            case R.id.btnCreacion:
+                startActivity(new Intent(this, CreacionActivity.class));
+                break;
+
+        }
+    }
+}
