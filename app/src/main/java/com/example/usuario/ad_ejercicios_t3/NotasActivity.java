e1279568ab4362b3698731c293ee2368277b38fe
ECasielles <flapspoiler@gmail.com>
2018-01-08 14:40:09 +0100

    Finish pending

--- app/src/main/java/com/example/usuario/ad_ejercicios_t3/NotasActivity.java
+++ app/src/main/java/com/example/usuario/ad_ejercicios_t3/NotasActivity.java
@@ -2,12 +2,29 @@ package com.example.usuario.ad_ejercicios_t3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class NotasActivity extends AppCompatActivity {
+
+    private TextView txvNotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notas);
+        txvNotas = findViewById(R.id.txvNotas);
+
+        try {
+            txvNotas.setText(Analisis.analizarXmlNextText(this));
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
 }
--- app/src/main/java/com/example/usuario/ad_ejercicios_t3/PartesActivity.java
+++ app/src/main/java/com/example/usuario/ad_ejercicios_t3/PartesActivity.java
@@ -2,12 +2,30 @@ package com.example.usuario.ad_ejercicios_t3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class PartesActivity extends AppCompatActivity {
+
+    public static final String TEXTO = "<texto><uno>Hello World!</uno><dos>Goodbye</dos></texto>";
+    private TextView txvPartes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partes);
+        txvPartes = findViewById(R.id.txvPartes);
+
+        try {
+            txvPartes.setText(Analisis.analizar(TEXTO));
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
